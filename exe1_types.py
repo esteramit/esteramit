7a621cc7ac837951ef458384c40ef042f45d8b84
Yonatan Amitay <yoniamity@gmail.com>
2021-10-20 01:07:51 +0300

    ass

--- exe1_types.py
+++ exe1_types.py
@@ -1,8 +1,9 @@
 name="Eran"   #str
 age=12.5     #float
-count_apples=8   #int
+count_apples=18   #int
 is_boy=True  #bool
-is_adult=False#?
+is_adulmt=False#?
 print(f"{name} is {age} years old. he has {count_apples} apples.")
 print(f"Is {name} a boy? {is_boy} he is!")
 
+print(8)
