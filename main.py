a14c1d208718318413898fafded2566d7249267d
Yonatan Amitay <yoniamity@gmail.com>
2021-10-20 00:38:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+x=7
+y="8-10"
+
+print("dan has",x,"apples")
+print("dan has"+str(x)+"apples")#no spaces
+print("dan has"+y+"apples")#y must be string
+print(f"dan has {y} apples")
+print(f"dan has {x} apples")
